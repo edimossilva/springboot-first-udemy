ff25604e436b48c18965e2866d03ad02f5c7231f
edimossilva <edimossilva@gmail.com>
2018-05-03 17:28:19 -0300

    first version

---
+++ src/main/java/com/loop/firstproject/FIrstProjectUdemyApplication.java
@@ -0,0 +1,12 @@
+package com.loop.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FIrstProjectUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FIrstProjectUdemyApplication.class, args);
+	}
+}
